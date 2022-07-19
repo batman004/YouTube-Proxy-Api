f1da4259b0c18f12c8618e887e4a2a69bffc873a
batman004 <pathania.yuvraj@gmail.com>
2022-07-19 10:30:27 +0530

    fix : Dockerfile configs

--- app/api/videos/auth/auth_helper.py
+++ app/api/videos/auth/auth_helper.py
@@ -10,22 +10,28 @@ api_keys = {}
 
 def api_key_auth(api_key: str = Depends(oauth2_scheme)):
     # check if api key in dict and if it is still valid
-    if (
-        api_key not in api_keys
-        or api_keys[api_key]["status"] == ApiKeySettings().INACTIVE
-    ):
-        print(api_keys[api_key]["status"])
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden"
-        )
+    try:
+        if (
+            api_key not in api_keys
+            or api_keys[api_key]["status"] == ApiKeySettings().INACTIVE
+        ):
+            print(api_keys[api_key]["status"])
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden"
+            )
 
-    return api_key
+        return api_key
+    except Exception as e:
+        return {"Exception": e}
 
 
 def invoke_api(api_key: str = Depends(api_key_auth)):
-    invoke_count = api_keys[api_key]["invoke_count"]
-    invoke_count += 1
+    try:
+        invoke_count = api_keys[api_key]["invoke_count"]
+        invoke_count += 1
 
-    api_keys[api_key]["invoke_count"] = invoke_count
-    if invoke_count > ApiKeySettings().THRESHOLD:
-        api_keys[api_key]["status"] = ApiKeySettings().INACTIVE
+        api_keys[api_key]["invoke_count"] = invoke_count
+        if invoke_count > ApiKeySettings().THRESHOLD:
+            api_keys[api_key]["status"] = ApiKeySettings().INACTIVE
+    except Exception as e:
+        print(e)
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+cd app
 docker build --tag=youtube-proxy-api .
-docker run --env-file ./app/.env --rm -it --name fampay-assignment -p 80:80 youtube-proxy-api
+docker run --env-file .env --rm -it --name fampay-assignment -p 80:80 youtube-proxy-api
