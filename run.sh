77c665c7bfeef05eca032bf2516f91db7fdb1e3a
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2022-08-28 02:15:39 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd app
 docker build --tag=youtube-proxy-api .
-docker run --env-file .env --rm -it --name fampay-assignment -p 80:80 youtube-proxy-api
+docker run --env-file .env --rm -it --name yt-api -p 80:80 youtube-proxy-api
